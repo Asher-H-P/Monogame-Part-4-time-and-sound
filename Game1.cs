38c3ccebd03d96e18d8414e8b375b0904ffb51f5
Asher Heywood Peters <339513541@GoTVDSB.ca>
2023-12-06 10:59:29 -0500

    Did some.

--- Game1.cs
+++ Game1.cs
@@ -8,7 +8,12 @@ namespace Monogame_Part_4_time_and_sound
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        Texture2D bombTexture;
+        private SpriteFont timer;
+        Rectangle bombRect;
+        float seconds;
+        float startTime;
+        MouseState mouseState;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -19,7 +24,11 @@ namespace Monogame_Part_4_time_and_sound
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            this.Window.Title = "bomb";
+            bombRect = new Rectangle(50, 50, 700, 400);
+            _graphics.PreferredBackBufferHeight = 500;
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.ApplyChanges();
             base.Initialize();
         }
 
@@ -28,6 +37,8 @@ namespace Monogame_Part_4_time_and_sound
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            bombTexture = Content.Load<Texture2D>("bomb");
+            timer = Content.Load<SpriteFont>("Writing");
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,15 +47,28 @@ namespace Monogame_Part_4_time_and_sound
                 Exit();
 
             // TODO: Add your update logic here
-
+            seconds = (float)gameTime.TotalGameTime.TotalSeconds - startTime;
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                startTime = (float)gameTime.TotalGameTime.TotalSeconds;
+            }
+            if (seconds == 10)
+            {
+                //End the program here.
+            }
+            mouseState = Mouse.GetState();
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.GreenYellow);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(bombTexture, bombRect, Color.White);
+            _spriteBatch.DrawString(timer, seconds.ToString("00.0"), new Vector2(300, 225), Color.Crimson);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
